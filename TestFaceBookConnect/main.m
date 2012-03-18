d53cec2a2164a3ee121e866259132b0e845831c7
superiocco <pablo_iocco@hotmail.com>
2012-03-17 22:32:04 -0300

    jijijijij

---
+++ TestFaceBookConnect/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Chris_Isle_of_Man_2
+//
+//  Created by Samuel Michelot on 10/01/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"Chris_Isle_of_Man_2AppDelegate");
+    [pool release];
+    return retVal;
+}
